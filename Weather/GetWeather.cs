b6f0643defc0b0c4f0f8c8c5d7a18dc35414ee31
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-09-21 13:04:51 -0700

    Add project files.

---
+++ Weather/GetWeather.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Weather
+{
+    class GetWeather
+    {
+        public static void Weather(string cityName)
+        {
+
+            var client = new HttpClient();
+            
+            string apiKey = "0f0b2208a39c49475a0497c2d52e9e38";
+            
+            string openWeatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+
+            string openWeatherResponse = client.GetStringAsync(openWeatherURL).Result;
+
+            string temps = JObject.Parse(openWeatherResponse).GetValue("main").ToString();
+
+            string condition = JObject.Parse(openWeatherResponse).GetValue("weather").ToString();
+
+            Console.WriteLine($"Today's Weather for {cityName}...");
+            Console.WriteLine($"Temps:  {temps}");
+            Console.WriteLine($"Condition:  {condition}");
+        }
+        
+    }
+}
---
+++ Weather/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello!  Welcome to what's my Weather App");
+
+            string cityName;
+            do
+            {
+                Console.WriteLine("Please enter the name of the city you would like to get the current weather conditions:  ");
+
+                cityName = Console.ReadLine().ToUpper();
+
+                if (cityName == "END")
+                {
+                    break;
+                }
+                GetWeather.Weather(cityName);
+
+            } while (cityName != "END");
+                
+
+            
+
+    
+
+            
+            
+        }
+    }
+}
